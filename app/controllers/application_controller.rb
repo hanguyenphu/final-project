188e22aec975466c5e7f34ff1b0cd67436231308
HaNguyen <hannguyenphu30@gmail.com>
2019-11-09 20:19:30 -0600

    Initialized Login and Sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
     include Pagy::Backend
+
+    protected
+
+    def configure_permitted_parameters
+         devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :address, :province_id, :email, :password)}
+
+         devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :address, :province_id, :email, :password, :current_password)}
+    end
 end
